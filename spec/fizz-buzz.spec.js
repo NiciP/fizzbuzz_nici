4b07e154dce9b42543c486e48266f42290c4e60a
Nici Putter <niciputter@yahoo.com>
2019-02-03 10:21:06 +0100

    FizzBuzz implementation added and tested

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -14,8 +14,10 @@ it('returns Fizz if number is divisible by 3', () => {
 
 it('returns Buzz if number is divisible by 5', () => {
     expect(fizzBuzz.check(5)).to.eql('Buzz')
-
 })
 
+it('returns FizzBuzz if number is divisible by 15', () => {
+    expect(fizzBuzz.check(15)).to.eql('FizzBuzz')
+})
 
 })
